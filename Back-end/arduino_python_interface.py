dbab89e055fccf4604c1e0f5f28514f8866af7d2
ajithkumar-natarajan <ajithkum@buffalo.edu>
2020-02-09 15:03:41 -0500

    Final files used for demo

--- Back-end/arduino_python_interface.py
+++ Back-end/arduino_python_interface.py
@@ -25,7 +25,7 @@ count=0
 while count<4:
     rawdata.append(str(arduino.readline().rstrip()))
     count+=1
-#print(rawdata)
+print(rawdata)
 
 #def clean(L):   #L is a list
 #    newl=[]     #initialising the new list
@@ -41,30 +41,34 @@ def write(L):
     data_dict = dict()
     data_dict['Temperature'] = float(L[0])
     data_dict['AirPressure'] = float(L[1])
+    if(L[2] == "Yes"):
+        L[2] = 1
+    else:
+        L[2] = 0
     data_dict['LightReplacement'] = L[2]
     data_dict['Grocery'] = L[3]
 
-    if os.path.isfile('../Front-end/data.json'):
-        count = 0
+    # if os.path.isfile('../Front-end/data.json'):
+    #     count = 0
 
-    else:
-        open('/Front-end/data_cumulative.txt', 'w') as f_cum:
-        f_cum.readline([cum])
+    # else:
+    #     with open('../Front-end/data_cumulative.txt', 'w') as f_cum:
+    #         f_cum.readline([cum])
 
-    if(int(cum[2]) < 31):
-        if(LightReplacement == "Yes"):
-            cum[0] = int(cum[0])+1
-        if(Grocery == "Yes"):
-            cum[1] = int(cum[1])+1
-    else:
-        if(LightReplacement == "Yes"):
-            cum[0] = 1
-        if(Grocery == "Yes"):
-            cum[1] = 1
-    if(count == 0):
-        cum[2] = 0
-    else:
-        cum[2] = int(cum[2]) + 1
+    # if(int(cum[2]) < 31):
+    #     if(LightReplacement == "Yes"):
+    #         cum[0] = int(cum[0])+1
+    #     if(Grocery == "Yes"):
+    #         cum[1] = int(cum[1])+1
+    # else:
+    #     if(LightReplacement == "Yes"):
+    #         cum[0] = 1
+    #     if(Grocery == "Yes"):
+    #         cum[1] = 1
+    # if(count == 0):
+    #     cum[2] = 0
+    # else:
+    #     cum[2] = int(cum[2]) + 1
 
     with open('../Front-end/data.json', 'w') as fp1:
         str_begin = "jsonstr = ["
