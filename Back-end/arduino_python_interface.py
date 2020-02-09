00286f656f81be436db266626e26dbb5f8f69f15
ajithkumar-natarajan <ajithkum@buffalo.edu>
2020-02-09 13:38:25 -0500

    Final files used for demo

--- Back-end/arduino_python_interface.py
+++ Back-end/arduino_python_interface.py
@@ -8,6 +8,8 @@ Created on Sun Feb 09 04:25:46 2020
 """Module importation"""
 import serial
 import json
+import os.path
+
 
 """Opening default serial port"""
 try:
@@ -41,7 +43,29 @@ def write(L):
     data_dict['AirPressure'] = float(L[1])
     data_dict['LightReplacement'] = L[2]
     data_dict['Grocery'] = L[3]
-    
+
+    if os.path.isfile('../Front-end/data.json'):
+        count = 0
+
+    else:
+        open('/Front-end/data_cumulative.txt', 'w') as f_cum:
+        f_cum.readline([cum])
+
+    if(int(cum[2]) < 31):
+        if(LightReplacement == "Yes"):
+            cum[0] = int(cum[0])+1
+        if(Grocery == "Yes"):
+            cum[1] = int(cum[1])+1
+    else:
+        if(LightReplacement == "Yes"):
+            cum[0] = 1
+        if(Grocery == "Yes"):
+            cum[1] = 1
+    if(count == 0):
+        cum[2] = 0
+    else:
+        cum[2] = int(cum[2]) + 1
+
     with open('../Front-end/data.json', 'w') as fp1:
         str_begin = "jsonstr = ["
         str_end = "];"
